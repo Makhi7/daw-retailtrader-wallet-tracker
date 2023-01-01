050220573b808da772b571989f16a6630c8f9023
Makhi7 <mvmangxola22@gmail.com>
2023-01-01 12:07:38 +0200

    adding spring app folders

---
+++ src/main/java/com/devatwork/traderwallettracker/TraderWalletTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.devatwork.traderwallettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderWalletTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderWalletTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devatwork/traderwallettracker/TraderWalletTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devatwork.traderwallettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraderWalletTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
